189bca3941c00ac0da33d0d9e88ec7f3b0be4064
sanseikanazawa <beck.ham@i.softbank.jp>
2018-10-04 18:00:28 +0900

    10/4

--- HelloServlet/src/TestServlet.java
+++ HelloServlet/src/TestServlet.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- HelloServlet/src/WelcomeServlet.java
+++ HelloServlet/src/WelcomeServlet.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,13 +25,13 @@ public class WelcomeServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/hhtml; charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 		String username=request.getParameter("username");
 
 		System.out.println(username);
 
 		PrintWriter out=response.getWriter();
 		out.println("<html><head></head><body><br>"+username+"さん、ようこそ！</body></html>");
-	}
 
+}
 }
